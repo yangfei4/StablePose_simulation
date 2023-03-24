3ff3ad071e435c4a219888458041ac069a81b89a
D-YF <daiyf1028@gmail.com>
2023-03-24 11:56:32 -0500

    script to generate video

--- generate_video.py
+++ generate_video.py
@@ -1,9 +1,13 @@
 import cv2
 import glob
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('image_path', default="./pose_exp/images",help="Path to the object file containing USB type-c components.")
+args = parser.parse_args()
 
 img_array = []
-path_list = glob.glob('./output/images/*.png')
+path_list = glob.glob(f'{args.image_path}/*.png')
 path_list.sort()
 
 for filename in path_list:
@@ -12,7 +16,7 @@ for filename in path_list:
     size = (width,height)
     img_array.append(img)
 
-out = cv2.VideoWriter('physical.avi',cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
+out = cv2.VideoWriter('sim_demo.avi',cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
 
 for i in range(len(img_array)):
     out.write(img_array[i])
